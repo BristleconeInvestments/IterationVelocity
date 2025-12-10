0806850f6e535e582de5aa5a5062e86b3630fa6c
Charles Selvaraj <charlesselvaraj@redtrilliumtechnologies.com>
2025-12-10 14:51:51 -0700

    Chore: Remove redundant empty line in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
 import './index.css'
-
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
